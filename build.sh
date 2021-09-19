527029d70b887f5badbd4c16140670ea6302e7fb
Kai Lyons <kailyons@pop-os.localdomain>
2021-09-19 16:28:31 -0600

    please just fucking work

--- build.sh
+++ build.sh
@@ -66,7 +66,7 @@ build(){
     -c "Potabi Live User" -d "/usr/home/${liveuser}"\
     -g wheel -G operator -m -s /bin/tcsh -k /usr/share/skel -w none
 
-    chroot ${release} mkdir -pv /home/${liveuser}/Desktop /home/${liveuser}/Documents /home/${liveuser}/Downloads /home/${liveuser}/Music /home/${liveuser}/Pictures /home/${liveuser}/Projects /home/${liveuser}/Videos
+    mkdir -pv ${release}/home/${liveuser}/Desktop ${release}/home/${liveuser}/Documents ${release}/home/${liveuser}/Downloads ${release}/home/${liveuser}/Music ${release}/home/${liveuser}/Pictures ${release}/home/${liveuser}/Projects ${release}/home/${liveuser}/Videos
 
     # Add desktop environment
     sed -i '' "s@#greeter-session=example-gtk-gnome@greeter-session=slick-greeter@" ${release}/usr/local/etc/lightdm/lightdm.conf
