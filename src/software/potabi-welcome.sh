abadaf38645578e480c504e2a7623ac4fa6bc822
Kai Lyons <kai@potabi.com>
2021-11-03 23:47:41 -0600

    make sure to make the directory

--- src/software/potabi-welcome.sh
+++ src/software/potabi-welcome.sh
@@ -1,5 +1,5 @@
 install_potabi-welcome(){
-    mkdir -pv
+    mkdir -pv ${release}/usr/local/potabi/welcome
     git clone https://github.com/Potabi/welcome ${release}/usr/local/potabi/welcome/. --depth 1
     chroot ${release} cd /usr/local/potabi/welcome && npm install
 }
