199b4a0dd61b73099b19e1e3cf2a941293e6de76
Kai Lyons <kai@potabi.com>
2021-11-04 16:19:48 -0600

    Update V installation

--- src/software/vlang.sh
+++ src/software/vlang.sh
@@ -1,5 +1,8 @@
 install_vlang(){
     mkdir -pv ${release}/usr/local/generic/v
     git clone https://github.com/vlang/v ${release}/usr/local/generic/v/. --depth 1
-    chroot ${release} cd /usr/local/generic/v && make && chmod 775 ./v && ./v symlink && cd /
+    cd ${release}/usr/local/generic/v/
+    make 
+    chmod 775 ${release}/usr/local/generic/v/v 
+    ln ${release}/usr/local/generic/v/v ${release}/bin/v
 }
