965f891e3e78b9e073ddfa854f61e52dbbfa4876
Kai Lyons <kai@potabi.com>
2021-11-03 20:46:48 -0600

    Just fucking work, ffs

--- src/software/potabi-welcome.sh
+++ src/software/potabi-welcome.sh
@@ -1,3 +1,4 @@
 install_potabi-welcome(){
-    chroot ${release} cp /usr/share/skel/Desktop/potabi-welcome.Desktop /home/${liveuser}/.config/autostart/potabi-welcome.desktop
+    # chroot ${release} cp /usr/share/skel/Desktop/potabi-welcome.Desktop /home/${liveuser}/.config/autostart/potabi-welcome.desktop
+    cp ${cwd}/uzip/potabi-welcome/files/usr/share/skel/Desktop/potabi-welcome.Desktop ${release}/home/${liveuser}/.config/autostart/potabi-welcome.desktop
 }
