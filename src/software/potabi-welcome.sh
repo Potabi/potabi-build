b773dade68f83fba48018f89a0c930519b7a5917
Kai Lyons <kai@potabi.com>
2021-11-04 16:10:39 -0600

    Fix Potabi Welcome install

--- src/software/potabi-welcome.sh
+++ src/software/potabi-welcome.sh
@@ -1,5 +1,6 @@
 install_potabi-welcome(){
     mkdir -pv ${release}/usr/local/potabi/welcome
     git clone https://github.com/Potabi/welcome ${release}/usr/local/potabi/welcome/. --depth 1
-    chroot ${release} cd /usr/local/potabi/welcome && npm install
+    cd ${release}/usr/local/potabi/welcome
+    npm install
 }
