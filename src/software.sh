25d1cd8ef04df20993cc9356ce9886a4de33e30c
Kai Lyons <kai@potabi.com>
2021-11-04 18:07:33 -0600

    Remove Potabi-build from script temporarily

--- src/software.sh
+++ src/software.sh
@@ -1,9 +1,10 @@
+# Potabi welcome is indefinitely on pause.
 . ${cwd}/src/software/vlang.sh 
-. ${cwd}/src/software/potabi-welcome.sh
+# . ${cwd}/src/software/potabi-welcome.sh
 . ${cwd}/src/software/doas.sh
 
 setup_software(){
-    install_potabi-welcome
+    # install_potabi-welcome
     install_vlang
     install_doas
 }
