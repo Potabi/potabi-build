90678f0975b2ca7089adbf16b80d4b1f82dd9ed1
Kai Lyons <kai@potabi.com>
2021-11-06 03:02:02 -0600

    Remove chroot from DOAS

--- src/software/doas.sh
+++ src/software/doas.sh
@@ -1,5 +1,7 @@
 install_doas(){
-    chroot ${release} touch /usr/local/etc/doas.conf && echo "permit nopass keepenv :wheel" >> /usr/local/etc/doas.conf && echo "permit nopass keepenv root as root" >> /usr/local/etc/doas.conf
+    touch ${release}/usr/local/etc/doas.conf
+    echo "permit nopass keepenv :wheel" >> ${release}/usr/local/etc/doas.conf
+    echo "permit nopass keepenv root as root" >> ${release}/usr/local/etc/doas.conf
     ln ${release}/usr/local/bin/doas ${release}/usr/local/bin/sudo
     ln ${release}/usr/local/bin/doas ${release}/usr/local/bin/admin
     ln ${release}/usr/local/bin/doas ${release}/usr/local/bin/root
