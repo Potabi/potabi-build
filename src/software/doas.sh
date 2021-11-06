71600ec03ee51b1124d9dbb2e4baff70bccc378d
Kai Lyons <kai@potabi.com>
2021-11-06 13:21:50 -0600

    /usr/local/etc does not exist fix

--- src/software/doas.sh
+++ src/software/doas.sh
@@ -1,4 +1,5 @@
 install_doas(){
+    mkdir -pv ${release}/usr/local/etc
     touch ${release}/usr/local/etc/doas.conf
     echo "permit nopass keepenv :wheel" >> ${release}/usr/local/etc/doas.conf
     echo "permit nopass keepenv root as root" >> ${release}/usr/local/etc/doas.conf
