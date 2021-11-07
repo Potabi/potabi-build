16a692c72c94123426f29a282f939a8a99403bc0
Kai Lyons <kai@potabi.com>
2021-11-06 20:58:18 -0600

    initial ion test

---
+++ src/software/ion.sh
@@ -0,0 +1,12 @@
+# Ion is the OFFICIAL shell for Potabi Systems
+# Beta-4 1.0A and later.
+install_ion(){
+    mkdir -pv ${release}/usr/local/tmp
+    cd ${release}/usr/local/tmp
+    fetch https://gitlab.redox-os.org/redox-os/ion/-/archive/master/ion-master.tar.gz
+    tar xf ion-master.tar.gz
+    cd ${release}/usr/local/tmp/ion-master 
+    chroot ${release} make RUSTUP=0
+    chroot ${release} make install prefix=/usr
+    chroot ${release} make update-shells prefix=/usr
+}
