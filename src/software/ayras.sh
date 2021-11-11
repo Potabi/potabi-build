610c3127743dbeb81b5762caeeb2da88d41d8a5c
Kai Lyons <kai@potabi.com>
2021-11-10 22:18:25 -0700

    use the qt5-qmake package

--- src/software/ayras.sh
+++ src/software/ayras.sh
@@ -1,6 +1,6 @@
 install_ayras(){
     git clone https://github.com/Potabi/ayras ${release}/usr/local/tmp/ayras
-    pkg install -y qmake trueos-libqt5 | true # Ensure Qmake is on the host system
+    pkg install -y qt5-qmake trueos-libqt5 | true # Ensure Qmake is on the host system
     cd ${release}/usr/local/tmp/ayras
     qmake PREFIX=${release}/usr/local LIBPREFIX=${release}/usr/local
     chroot ${release} ls
