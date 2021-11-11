dcd2eeeb583fd5c1a238bcf4b0de163130b813e1
Kai Lyons <kai@potabi.com>
2021-11-10 22:29:35 -0700

    fix command

--- src/software/ayras.sh
+++ src/software/ayras.sh
@@ -2,6 +2,6 @@ install_ayras(){
     git clone https://github.com/Potabi/ayras ${release}/usr/local/tmp/ayras
     pkg install -y qt5-qmake trueos-libqt5 | true # Ensure Qmake is on the host system
     cd ${release}/usr/local/tmp/ayras
-    qmake PREFIX=${release}/usr/local LIBPREFIX=${release}/usr/local
+    qmake-qt5 PREFIX=${release}/usr/local LIBPREFIX=${release}/usr/local
     chroot ${release} ls
 }
