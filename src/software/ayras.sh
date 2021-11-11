97f5c6f46eb4e431fbc0024c08fef6aeb02e6045
Kai Lyons <kai@potabi.com>
2021-11-11 00:12:35 -0700

    Go back to original qmake and ls - debugging

--- src/software/ayras.sh
+++ src/software/ayras.sh
@@ -1,8 +1,8 @@
 install_ayras(){
     git clone https://github.com/Potabi/ayras ${release}/usr/local/tmp/ayras
-    pkg install -y qt5-qmake trueos-libqt5 | true # Ensure Qmake is on the host system
+    pkg install -y qt5-qmake qmake trueos-libqt5 | true # Ensure Qmake is on the host system
     cd ${release}/usr/local/tmp/ayras
-    chroot ${release} cd /usr/local/tmp/ayras && qmake-qt5
+    chroot ${release} cd /usr/local/tmp/ayras && qmake && ls
     chroot ${release} cd /usr/local/tmp/ayras && make && ls
     chroot ${release} cd /usr/local/tmp/ayras && make install && ls
     exit 1
