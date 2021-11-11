b72999d6a8a97605cbe2c2df9d084cfb4cb113c1
Kai Lyons <kai@potabi.com>
2021-11-10 21:49:08 -0700

    Move from fetch to git

--- src/software/ayras.sh
+++ src/software/ayras.sh
@@ -1,9 +1,7 @@
 install_ayras(){
-    cd ${release}/usr/local/tmp
-    fetch https://github.com/Potabi/ayras/archive/refs/tags/v1.6.1.tar.gz
+    git clone https://github.com/Potabi/ayras ${release}/usr/local/tmp/ayras
     pkg install -y qmake | true # Ensure Qmake is on the host system
-    tar xf v1.6.1.tar.gz
-    cd ayras-v1.6.1
+    cd ${release}/usr/local/tmp/ayras
     qmake PREFIX=${release}/usr/local LIBPREFIX=${release}/usr/local
     chroot ${release} ls
 }
