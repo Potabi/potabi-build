404861403016c7ecf5d44d9963d0f7684b6198a7
Kai Lyons <kai@potabi.com>
2021-11-10 23:22:02 -0700

    Add make and make install - still debugging

--- src/software/ayras.sh
+++ src/software/ayras.sh
@@ -3,6 +3,7 @@ install_ayras(){
     pkg install -y qt5-qmake trueos-libqt5 | true # Ensure Qmake is on the host system
     cd ${release}/usr/local/tmp/ayras
     qmake-qt5 PREFIX=${release}/usr/local LIBPREFIX=${release}/usr/local
-    chroot ${release} cd /usr/local/tmp/ayras && ls
+    chroot ${release} cd /usr/local/tmp/ayras && make
+    chroot ${release} cd /usr/local/tmp/ayras && make install
     exit 1
 }
