b9f8310c971fb360feb72ffc3f018d4410525998
Kai Lyons <kai@potabi.com>
2021-11-10 23:57:39 -0700

    add ls to make - debugging

--- src/software/ayras.sh
+++ src/software/ayras.sh
@@ -3,7 +3,7 @@ install_ayras(){
     pkg install -y qt5-qmake trueos-libqt5 | true # Ensure Qmake is on the host system
     cd ${release}/usr/local/tmp/ayras
     chroot ${release} cd /usr/local/tmp/ayras && qmake-qt5
-    chroot ${release} cd /usr/local/tmp/ayras && make
+    chroot ${release} cd /usr/local/tmp/ayras && make && ls
     chroot ${release} cd /usr/local/tmp/ayras && make install && ls
     exit 1
 }
