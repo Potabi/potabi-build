a6e7ab16b08ee264e35f67d720c1333073b77d1c
Kai Lyons <kai@potabi.com>
2021-11-18 16:27:41 -0700

    Directories do not exist

--- src/software/ayras.sh
+++ src/software/ayras.sh
@@ -1,6 +1,8 @@
 install_ayras(){
     git clone https://github.com/Potabi/ayras ${release}/usr/local/tmp/ayras
     pkg install -y qt5-qmake qmake trueos-libqt5 | true # Ensure Qmake is on the host system
+    mkdir -pv /usr/lib/qt5
+    mkdir -pv /usr/lib/qt5/bin
     ln /usr/local/bin/qmake /usr/lib/qt5/bin/qmake
     ln ${release}/usr/local/bin/qmake ${release}/usr/lib/qt5/bin/qmake
     cd ${release}/usr/local/tmp/ayras
