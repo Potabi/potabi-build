a3ac9b6e15603b522949644b8afddf4a953c9f4f
Kai Lyons <kai@potabi.com>
2021-11-18 16:53:59 -0700

    Do the same for ${release}

--- src/software/ayras.sh
+++ src/software/ayras.sh
@@ -3,6 +3,8 @@ install_ayras(){
     pkg install -y qt5-qmake qmake trueos-libqt5 | true # Ensure Qmake is on the host system
     mkdir -pv /usr/lib/qt5
     mkdir -pv /usr/lib/qt5/bin
+    mkdir -pv ${release}/usr/lib/qt5
+    mkdir -pv ${release}/usr/lib/qt5/bin
     ln /usr/local/bin/qmake /usr/lib/qt5/bin/qmake
     ln ${release}/usr/local/bin/qmake ${release}/usr/lib/qt5/bin/qmake
     cd ${release}/usr/local/tmp/ayras
