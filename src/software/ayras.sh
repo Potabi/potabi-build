98aacc1ed9c61941a185fb73e1b6e98f72f388cc
Kai Lyons <kai@potabi.com>
2021-11-19 17:23:29 -0700

    oops syntax error

--- src/software/ayras.sh
+++ src/software/ayras.sh
@@ -5,7 +5,7 @@ install_ayras(){
     mkdir -pv /usr/lib/qt5/bin
     mkdir -pv ${release}/usr/lib/qt5
     mkdir -pv ${release}/usr/lib/qt5/bin
-    setenv QMAKESPEC freebsd-g++
+    setenv QMAKESPEC=freebsd-g++
     ln ${release}/usr/local/bin/qmake ${release}/usr/lib/qt5/bin/qmake
     cd ${release}/usr/local/tmp/ayras
     chroot ${release} cd /usr/local/tmp/ayras && qmake && ls
