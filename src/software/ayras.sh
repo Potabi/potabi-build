0b9e6832d6d3bb7b4be1d535ca3fd8dfb0dbd141
Kai Lyons <kai@potabi.com>
2021-11-19 20:57:31 -0700

    The closest we have gotten to success

--- src/software/ayras.sh
+++ src/software/ayras.sh
@@ -7,7 +7,7 @@ install_ayras(){
     mkdir -pv ${release}/usr/lib/qt5/bin
     ln ${release}/usr/local/bin/qmake ${release}/usr/lib/qt5/bin/qmake
     cd ${release}/usr/local/tmp/ayras
-    chroot ${release} cd /usr/local/tmp/ayras && qmake -spec freebsd-g++ && ls
+    chroot ${release} cd /usr/local/tmp/ayras && qmake-qt5 -spec freebsd-g++ && ls
     chroot ${release} cd /usr/local/tmp/ayras && make && ls
     chroot ${release} cd /usr/local/tmp/ayras && make install && ls
     exit 1
