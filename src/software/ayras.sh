f1f96e832226949a218354e627a5375b929f9eea
Kai Lyons <loralighte@pop-os.localdomain>
2021-11-28 05:28:17 -0700

    Undo changes to do research on solutions

--- src/software/ayras.sh
+++ src/software/ayras.sh
@@ -1,8 +1,8 @@
 install_ayras(){
     # Yes, I know I fucked up on the release numbers when making these. 
     # Oh well. At least we know for a while that these have to be the 
-    # packages I made because only I am stupid enough to let this happen.
+    # packages I made because only I am stupid enough to let this happen
 
-    pkg -c ${release} add ${sftdir}/pkg/ayras-coreutils-1.6.3.pkg
-    pkg -c ${release} add ${sftdir}/pkg/lumina-core-1.6.3_1.pkg
+    pkg -c ${release} install -y ${sftdir}/pkg/ayras-coreutils-1.6.3.pkg
+    pkg -c ${release} install -y ${sftdir}/pkg/lumina-core-1.6.3_1.pkg
 }
